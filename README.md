fdb8184174f17ceaae9708356e244ba88db5b48d
Daniel A <dreth+services@protonmail.com>
2021-03-21 15:56:07 +0100

    homepage and some styling
    Co-authored-by: danyuz <danyuz@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # DashTakeaway
+
 Dash project fro Data Tidying and Reporting
 
 ## Group members
@@ -6,6 +7,6 @@ Dash project fro Data Tidying and Reporting
 - [Danyu Zhang](https://github.com/danyuz)
 - [Daniel Alonso](https://github.com/dreth)
 
-### Boilerplate code source
+## Boilerplate code source
 
 The boilerplate sidebar layout for this dash page was obtained from the following [dash-bootstrap-components example](https://github.com/facultyai/dash-bootstrap-components/blob/main/examples/multi-page-apps/simple_sidebar.py).
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+from os import name
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -12,7 +13,9 @@ external_stylesheets = [
     'https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/cyborg/bootstrap.min.css']
 
 # Dash app initialization
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, title='Multiple dataset Dash app')
+
+# %% LAYOUT STYLES
 
 # Sidebar style arguments
 SIDEBAR_STYLE = {
@@ -34,21 +37,52 @@ CONTENT_STYLE = {
     "padding": "2rem 1rem"
 }
 
+# %% APP LAYOUT
+
+# Iterative generator of page navlinks for development dataset
+sidebar_tabs_dev = ['Development dataset', 'Histograms',
+                    'Boxplots', 'Correlation', 'HDI Aggregates', 'Top N countries']
+
+
+# Iterative generator of page navlinks for ramen ratings dataset
+sidebar_tabs_ram = ['Ramen ratings', 'Top 10 by Rating',
+                    'Top 10 with details', 'Regression for Ratings']
+
+# function to convert sidebar tab names into sidebar tabs
+
+
+def sidebar_tabs(tabnames, active='exact', external_link=True):
+    navlinks = [0 for x in tabnames]
+    for i in range(len(tabnames)):
+        # params
+        tabname = tabnames[i]
+        href_add = f"/{tabname.lower().replace(' ','-')}"
+
+        # generating the list of navlinks
+        navlink = dbc.NavLink(tabname.lower(),
+                                href=href_add,
+                                active=active,
+                                external_link=external_link)
+        navlinks[i] = navlink
+    return navlinks
+
+
 # Sidebar
 sidebar = html.Div(
     [
-        html.H3("The development dataset", className="display-6"),
+        html.H3("Dash App", className="fs-4", id='sideBarTitle'),
         html.Hr(),
+        html.H5("The development dataset", className="fs-5 sideBarDatasetTitles"),
         # Navbar with main links
         dbc.Nav(
-            [
-                dbc.NavLink("Home", href="/", active="exact",
-                            external_link=True),
-                dbc.NavLink("Page 1", href="/page-1",
-                            active="exact", external_link=True),
-                dbc.NavLink("Page 2", href="/page-2",
-                            active="exact", external_link=True),
-            ],
+            sidebar_tabs(sidebar_tabs_dev),
+            vertical=True,
+            pills=True,
+        ),
+        html.Hr(),
+        html.H5("Ramen ratings", className="fs-5 sideBarDatasetTitles"),
+        dbc.Nav(
+            sidebar_tabs(sidebar_tabs_ram),
             vertical=True,
             pills=True,
         ),
@@ -56,29 +90,60 @@ sidebar = html.Div(
     style=SIDEBAR_STYLE,
 )
 
+# content div
 content = html.Div(id="page-content", style=CONTENT_STYLE)
 
+# app general layout
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+# %% PAGE CONTENT
 
 
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
-    if pathname == "/":
+
+    # root directory / homepage
+    if pathname == '/':
+        return dbc.Jumbotron([
+            html.H2("Welcome to our Dash App!", id='mainTitle'),
+            html.P("The developers of this app are:"),
+            html.Ul([
+                html.Li(html.A("Danyu Zhang",href='https://github.com/danyuz')),
+                html.Li(html.A("Daniel Alonso",href='https://github.com/dreth'))
+            ]),
+            html.P("This application uses 2 datasets, specific information about each dataset is detailed in the following links:"),
+            html.Ul([
+                html.Li(html.A("The development dataset",href='/The-dataset')),
+                html.Li(html.A("Ramen ratings dataset",href='/Introduction'))
+            ]),
+            html.P("Feel free to navigate the page and play around with the menus!")
+        ])
+
+    # DEVELOPMENT DATASET PAGES
+    elif pathname == "/development-dataset":
+        return html.Div([
+            html.H3(
+                "A set of visualizations for different worldwide demographic/development metrics")])
+
+
+    # RAMEN DATASET PAGES
+    elif pathname == "/ramen-ratings":
         return html.P("This is the content of the home page!")
-    elif pathname == "/page-1":
-        return html.P("This is the content of page 1. Yay!")
-    elif pathname == "/page-2":
-        return html.P("Oh cool, this is page 2!")
 
-    # 404
-    return dbc.Jumbotron(
-        [
-            html.H1("404: Not found", className="text-danger"),
-            html.Hr(),
-            html.P(f"The path {pathname} does not exist"),
-        ]
-    )
+    
+    # 404 ERROR MESSAGE PAGE
+    else:
+        return dbc.Jumbotron(
+            [
+                html.H1("404: Not found", className="text-danger"),
+                html.Hr(),
+                html.P(f"The path {pathname} does not exist"),
+            ]
+        )
+
+# %% SERVER
 
 
+# run server
 if __name__ == "__main__":
     app.run_server(port=5665)
